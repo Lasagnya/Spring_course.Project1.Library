8ad9e1ad5b5f9b36668d8323fcae2b742ad43fdb
Lasagnya <stepik-2003@yandex.ru>
2023-07-31 19:10:49 +0300

    Add all book job.

--- src/main/java/springcourse/controllers/BooksController.java
+++ src/main/java/springcourse/controllers/BooksController.java
@@ -1,15 +1,79 @@
 package springcourse.controllers;
 
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import springcourse.dao.BookDAO;
+import springcourse.models.Book;
 
 @Controller
 @RequestMapping("/books")
 public class BooksController {
+	private final BookDAO bookDAO;
+
+	@Autowired
+	public BooksController(BookDAO bookDAO) {
+		this.bookDAO = bookDAO;
+	}
+
 	@GetMapping()
 	public String index(Model model) {
-		return "/books/index";
+		model.addAttribute("books", bookDAO.index());
+		return "books/index";
+	}
+
+	@GetMapping("/{id}")
+	public String show(Model model, @PathVariable("id") int id) {
+		if (bookDAO.show(id).isPresent()) {
+			model.addAttribute("book", bookDAO.show(id).get());
+			return "books/show";
+		}
+		else {
+			model.addAttribute("id", id);
+			return "books/incorrect_id";
+		}
+	}
+
+	@GetMapping("/new")
+	public String newBook(Model model) {
+		model.addAttribute("book", new Book());
+		return "books/new";
+	}
+
+	@PostMapping()
+	public String create(@ModelAttribute("book") @Valid Book book, BindingResult bindingResult) {
+		if(bindingResult.hasErrors())
+			return "books/new";
+		bookDAO.save(book);
+		return "redirect:/books";
+	}
+
+	@GetMapping("/{id}/edit")
+	public String edit(@PathVariable("id") int id, Model model) {
+		if (bookDAO.show(id).isPresent()) {
+			model.addAttribute("book", bookDAO.show(id).get());
+			return "books/edit";
+		}
+		else {
+			model.addAttribute("id", id);
+			return "books/incorrect_id";
+		}
+	}
+
+	@PatchMapping("/{id}")
+	public String update(@PathVariable("id") int id, @ModelAttribute("book") @Valid Book book, BindingResult bindingResult) {
+		if(bindingResult.hasErrors())
+			return "books/edit";
+		bookDAO.update(id, book);
+		return "redirect:/books";
+	}
+
+	@DeleteMapping("/{id}")
+	public String delete(@PathVariable("id") int id) {
+		bookDAO.delete(id);
+		return "redirect:/books";
 	}
 }
--- src/main/java/springcourse/models/Book.java
+++ src/main/java/springcourse/models/Book.java
@@ -7,11 +7,9 @@ import jakarta.validation.constraints.Size;
 public class Book {
 	private int id;
 
-	@NotEmpty(message = "Title should not be empty.")
 	@Size(min = 2, max = 100, message = "Title should be between 2 and 100 characters")
 	private String title;
 
-	@NotEmpty(message = "Author should not be empty.")
 	@Size(min = 3, max = 50, message = "Author should be between 3 and 50 characters")
 	private String author;
 
