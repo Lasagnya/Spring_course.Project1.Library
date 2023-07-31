60075bfc00ea516a4cdfac02513bffa14c6c49fc
Lasagnya <stepik-2003@yandex.ru>
2023-07-31 13:49:35 +0300

    Add UTF-8 for string fields and patch name error.

--- src/main/java/springcourse/models/Person.java
+++ src/main/java/springcourse/models/Person.java
@@ -8,7 +8,6 @@ import jakarta.validation.constraints.Size;
 public class Person {
 	private int id;
 
-	@NotEmpty(message = "Name should not be empty.")
 	@Size(min = 3, max = 50, message = "Name should be between 3 and 50 characters")
 	private String name;
 
